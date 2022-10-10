d1af0e7278d9188e8075952508030d44c881a7f0
JMLBoeing747 <jml514@hotmail.com>
2022-10-09 21:22:24 -0400

    Correction : Conversion des structures en classes pour assurer leur mutabilité (#5)

--- LSZ/LORENZ/Compression.cs
+++ LSZ/LORENZ/Compression.cs
@@ -2,85 +2,67 @@
 
 namespace LORENZ
 {
-    public static class Compression
+    public class SubWord
     {
-        private struct SubWord
+        public string WordCode { get; }
+        public int Count { get; private set; }
+
+        public SubWord(string wordCode)
         {
-            public string WordCode { get; }
-            public int Count { get; private set; }
+            WordCode = wordCode;
+            Count = 1;
+        }
 
-            public SubWord(string wordCode)
+        public void Repeat(string newWord)
+        {
+            if (newWord == WordCode)
             {
-                WordCode = wordCode;
-                Count = 1;
-            }
-
-            public void Repeat(string newWord)
-            {
-                if (newWord == WordCode)
-                {
-                    Count++;
-                }
+                Count++;
             }
         }
-        private struct WordEntry
+    }
+    public class WordEntry
+    {
+        public string MainWord { get; private set; }
+        private readonly List<SubWord> SubWordsList;
+
+        public WordEntry(string entry)
         {
-            public string MainWord { get; private set; }
-            private readonly List<SubWord> SubWordsList;
+            SubWordsList = new();
+            MainWord = entry.ToLower();
+            string entryCode = EncodeWord(entry);
+            SubWord sw = new(entryCode);
+            SubWordsList.Add(sw);
+        }
 
-            public WordEntry(string entry)
+        public int CountAll
+        {
+            get
             {
-                SubWordsList = new();
-                MainWord = entry.ToLower();
-                string entryCode = EncodeWord(entry);
-                SubWord sw = new(entryCode);
-                SubWordsList.Add(sw);
-            }
-
-            public int CountAll
-            {
-                get
+                int temp = 0;
+                foreach (SubWord item in SubWordsList)
                 {
-                    int temp = 0;
-                    foreach (SubWord item in SubWordsList)
-                    {
-                        temp += item.Count;
-                    }
-
-                    return temp;
+                    temp += item.Count;
                 }
+
+                return temp;
             }
+        }
 
-            public bool Add(string entry)
+        public bool Add(string entry)
+        {
+            string[] divideWord = entry.Split('\'');
+            if (divideWord.Length == 2)
             {
-                string[] divideWord = entry.Split('\'');
-                if (divideWord.Length == 2)
+                string bigWord = divideWord[0].Length > divideWord[1].Length ? divideWord[0] : divideWord[1];
+                if (bigWord.ToLower() != MainWord)
                 {
-                    string bigWord = divideWord[0].Length > divideWord[1].Length ? divideWord[0] : divideWord[1];
-                    if (bigWord.ToLower() != MainWord)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        string littleWord = divideWord[0] == bigWord ? "\x90" + divideWord[1] : divideWord[0] + "\x90";
-                        string entryCode = EncodeWord(bigWord, littleWord);
-                        foreach (SubWord item in SubWordsList)
-                        {
-                            if (item.WordCode == entryCode)
-                            {
-                                item.Repeat(entryCode);
-                                return true;
-                            }
-                        }
-
-                        SubWord sw = new(entryCode);
-                        SubWordsList.Add(sw);
-                    }
+                    return false;
                 }
-                else if (entry.ToLower() == MainWord)
+                else
                 {
-                    string entryCode = EncodeWord(entry);
+                    string littleWord = divideWord[0] == bigWord ? "\x90" + divideWord[1] : divideWord[0] + "\x90";
+                    string entryCode = EncodeWord(bigWord, littleWord);
                     foreach (SubWord item in SubWordsList)
                     {
                         if (item.WordCode == entryCode)
@@ -93,94 +75,112 @@ namespace LORENZ
                     SubWord sw = new(entryCode);
                     SubWordsList.Add(sw);
                 }
+            }
+            else if (entry.ToLower() == MainWord)
+            {
+                string entryCode = EncodeWord(entry);
+                foreach (SubWord item in SubWordsList)
+                {
+                    if (item.WordCode == entryCode)
+                    {
+                        item.Repeat(entryCode);
+                        return true;
+                    }
+                }
+
+                SubWord sw = new(entryCode);
+                SubWordsList.Add(sw);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private string EncodeWord(string word, string part = null)
+        {
+            string entryCode = default;
+            for (int cWord = 0; cWord < word.Length; cWord++)
+            {
+                if (MainWord[cWord] != word[cWord])
+                {
+                    entryCode += "C";
+                }
                 else
                 {
-                    return false;
+                    entryCode += "L";
                 }
-
-                return true;
             }
 
-            private string EncodeWord(string word, string part = null)
+            if (entryCode == "C" + new string('L', entryCode.Length - 1))
             {
-                string entryCode = default;
-                for (int cWord = 0; cWord < word.Length; cWord++)
-                {
-                    if (MainWord[cWord] != word[cWord])
-                    {
-                        entryCode += "C";
-                    }
-                    else
-                    {
-                        entryCode += "L";
-                    }
-                }
-
-                if (entryCode == "C" + new string('L', entryCode.Length - 1))
-                {
-                    entryCode = "T";
-                }
-                else if (entryCode == new string('L', entryCode.Length))
-                {
-                    entryCode = "";
-                }
-                else if (entryCode == new string('C', entryCode.Length))
-                {
-                    entryCode = "U";
-                }
-
-                if (part != null)
-                {
-                    if (entryCode == "")
-                    {
-                        entryCode += "X" + part;
-                    }
-                    else
-                    {
-                        entryCode += part;
-                    }
-                }
-
-                return entryCode;
+                entryCode = "T";
             }
+            else if (entryCode == new string('L', entryCode.Length))
+            {
+                entryCode = "";
+            }
+            else if (entryCode == new string('C', entryCode.Length))
+            {
+                entryCode = "U";
+            }
+
+            if (part != null)
+            {
+                if (entryCode == "")
+                {
+                    entryCode += "X" + part;
+                }
+                else
+                {
+                    entryCode += part;
+                }
+            }
+
+            return entryCode;
+        }
+    }
+
+    public class CompressTable
+    {
+        private readonly List<WordEntry> WordsList;
+
+        public CompressTable()
+        {
+            WordsList = new();
         }
 
-        private struct CompressTable
+        public void Check(string newWord)
         {
-            private readonly List<WordEntry> WordsList;
-
-            public CompressTable()
+            if (newWord.Length > 2)
             {
-                WordsList = new List<WordEntry>();
-            }
-
-            public void Check(string newWord)
-            {
-                if (newWord.Length > 2)
+                if (WordsList.Count > 0)
                 {
-                    if (WordsList.Count > 0)
+                    foreach (WordEntry we in WordsList)
                     {
-                        foreach (WordEntry we in WordsList)
+                        if (we.Add(newWord))
                         {
-                            if (we.Add(newWord))
-                            {
-                                break;
-                            }
-                            else if (WordsList.IndexOf(we) == WordsList.Count - 1)
-                            {
-                                WordsList.Add(new(newWord));
-                                break;
-                            }
+                            break;
+                        }
+                        else if (WordsList.IndexOf(we) == WordsList.Count - 1)
+                        {
+                            WordsList.Add(new(newWord));
+                            break;
                         }
                     }
-                    else
-                    {
-                        WordsList.Add(new(newWord));
-                    }
+                }
+                else
+                {
+                    WordsList.Add(new(newWord));
                 }
             }
         }
+    }
 
+    public static class Compression
+    {
         public static void TryCompression(string strToCompress)
         {
             List<string> words = new();
