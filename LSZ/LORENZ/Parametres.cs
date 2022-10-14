75be2df95f026c647eec7ca659a07ec608bd977e
JMLBoeing747 <jml514@hotmail.com>
2022-10-14 12:43:59 -0400

    Ajout : Inscription du ratio de compression dans le fichier .ini

--- LSZ/LORENZ/Parametres.cs
+++ LSZ/LORENZ/Parametres.cs
@@ -308,7 +308,7 @@ namespace LORENZ
             sb.Clear();
 
             int ini4 = GetPrivateProfileString(setting, "CMPRSRATIO", "", sb, sb.Capacity, GeneralParamsFile);
-            // Insert ratio code assignment
+            Compression.RatioCompressionMin = double.TryParse(sb.ToString(), out double ratio) ? ratio : 0.15;
         }
 
         public static void WriteGeneralParamsFile()
@@ -318,7 +318,7 @@ namespace LORENZ
             WritePrivateProfileString(profile, "PSEUDONAME", PseudoName, GeneralParamsFile);
             string setting = "Setting";
             WritePrivateProfileString(setting, "CIPHFILEDR", CipherFileDirectory, GeneralParamsFile);
-            WritePrivateProfileString(setting, "CMPRSRATIO", "0.15", GeneralParamsFile); // Insérer la variable ratio
+            WritePrivateProfileString(setting, "CMPRSRATIO", Compression.RatioCompressionMin.ToString(), GeneralParamsFile);
         }
     }
 }
